9f398cd5488d62cde948a964d57bf16a5395152f
Pedro Vivas <pmvp82@gmail.com>
2020-04-27 19:28:49 -0400

    Updates

---
+++ precache-manifest.11242f46156eb5fc3c8eac29aad9d29b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bd7627dd97955e614d79c8132dcdab",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "a4fce7f0ad8cc1715eec",
+    "url": "/simple-todo-app/static/css/main.645525b1.chunk.css"
+  },
+  {
+    "revision": "cf0a7f97a38e75577ded",
+    "url": "/simple-todo-app/static/js/2.0ae6a4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.0ae6a4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fce7f0ad8cc1715eec",
+    "url": "/simple-todo-app/static/js/main.f23d0197.chunk.js"
+  },
+  {
+    "revision": "d3f8aceb317e5aa1253f",
+    "url": "/simple-todo-app/static/js/runtime-main.6f5ce6f0.js"
+  }
+]);
